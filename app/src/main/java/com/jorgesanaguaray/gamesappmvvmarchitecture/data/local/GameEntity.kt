46139a0260d8a8a783582447dc2538d5ce815acd
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-10-16 14:35:34 -0500

    I have added the data mapping from: GameItem to GameEntity.

--- app/src/main/java/com/jorgesanaguaray/gamesappmvvmarchitecture/data/local/GameEntity.kt
+++ app/src/main/java/com/jorgesanaguaray/gamesappmvvmarchitecture/data/local/GameEntity.kt
@@ -2,6 +2,7 @@ package com.jorgesanaguaray.gamesappmvvmarchitecture.data.local
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.jorgesanaguaray.gamesappmvvmarchitecture.domain.GameItem
 
 /**
  * Created by Jorge Sanaguaray
@@ -16,3 +17,5 @@ data class GameEntity(
     val thumbnail: String
 
 )
+
+fun GameItem.toDatabase() = GameEntity(id = id, title = title, thumbnail = thumbnail)
